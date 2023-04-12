fbbea23a43b906f38e24610bf89272845d4bfc35
Frische <paul.frische@adesso.de>
2023-04-12 15:16:51 +0200

    add support for binary questions

---
+++ src/main/java/org/paulfrische/InputUtil.java
@@ -0,0 +1,19 @@
+package org.paulfrische;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    private Scanner stdin;
+    public InputUtil(Scanner stdin) {
+        this.stdin = stdin;
+    }
+    public boolean binaryQuestion(String prompt) {
+        System.out.print(prompt + "\n[Y/n] ");
+        String input = "";
+        input = stdin.nextLine();
+        if (input.toLowerCase() == "y" || input == "") {
+            return true;
+        }
+        return false;
+    }
+}

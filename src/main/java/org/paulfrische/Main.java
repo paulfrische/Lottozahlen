a663289bd0d88f2da166e24973e203466e4fcb90
Frische <paul.frische@adesso.de>
2023-04-12 15:17:35 +0200

    add support for generating multiple number sets in one session

--- src/main/java/org/paulfrische/Main.java
+++ src/main/java/org/paulfrische/Main.java
@@ -10,19 +10,33 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner stdin = new Scanner(System.in);
+    private static InputUtil input = new InputUtil(stdin);
     public static void main(String[] args) throws Exception {
+        List<Short> badNumbers = getBadNumbersCached(args);
+        boolean run = true;
+        while (run) {
+            Game game = new ClassicGame(badNumbers);
+            for (short num : game.generateNumbers()) {
+                System.out.print(num + " ");
+            }
+            System.out.println("");
+            run = input.binaryQuestion("Do you want to generate more numbers?");
+            if (run) {
+                badNumbers = getBadNumbers();
+            }
+        }
+        stdin.close();
+    }
+
+    public static List<Short> getBadNumbersCached(String[] args) throws Exception {
         File file = new File("numbers.json");
-        Scanner stdin = new Scanner(System.in);
         Gson gson = new Gson();
         List<Short> badNumbers = new ArrayList();
         boolean reuse = false;
 
         if (file.exists() && args.length == 0) {
-            System.out.print("There are old numbers, do you want to reuse them?\n[Y/n] ");
-            String input = stdin.nextLine();
-            if (input.toLowerCase() == "y" || input == "") {
-                reuse = true;
-            }
+            reuse = input.binaryQuestion("There are old numbers, do you want to reuse them?");
         }
 
         if (!reuse) {
@@ -51,10 +65,26 @@ public class Main {
             String data = reader.nextLine();
             TypeToken<ArrayList<Short>> shortList = new TypeToken<>(){};
             badNumbers = gson.fromJson(data, shortList);
+            reader.close();
         }
-        Game game = new ClassicGame(badNumbers);
-        for (short num : game.generateNumbers()) {
-            System.out.print(num + " ");
-        }
+        return badNumbers;
+    }
+
+    public static List<Short> getBadNumbers() throws Exception {
+        List<Short> badNumbers = new ArrayList();
+
+        List<String> arguments = new ArrayList<>();
+
+            System.out.println("Enter your numbers (enter q to finish entering numbers)");
+            for (int i = 0; i < 6; i++) {
+                System.out.print("[" + i + "] number: ");
+                String input = stdin.nextLine();
+                if (input.equals("q"))
+                    break;
+                arguments.add(input);
+            }
+        ArgumentParser parser = new ArgumentParser(arguments);
+        badNumbers = parser.numbers(6);
+        return badNumbers;
     }
 }

753f50161edd9d408a74f7a5ded0ddbfb2607ad1
Frische <paul.frische@adesso.de>
2023-04-12 13:19:03 +0200

    add support for saving numbers

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+
+    implementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
--- src/main/java/org/paulfrische/Main.java
+++ src/main/java/org/paulfrische/Main.java
@@ -1,28 +1,47 @@
 package org.paulfrische;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        List<String> arguments = new ArrayList<>();
-        if (args.length > 0){
-             arguments = new ArrayList<>(List.of(args));
-        } else {
-            System.out.println("Enter your numbers (enter q to finish entering numbers)");
-            Scanner scanner = new Scanner(System.in);
-            List<String> numbers = new ArrayList<>();
-            for (int i = 0; i < 6; i++) {
-                System.out.print("[" + i + "] number: ");
-                String input = scanner.nextLine();
-                if (input.equals("q"))
-                    break;
-                numbers.add(input);
+        File file = new File("numbers.json");
+        Gson gson = new Gson();
+        List<Short> badNumbers = new ArrayList();
+        if (!file.exists()) {
+            List<String> arguments = new ArrayList<>();
+            if (args.length > 0){
+                arguments = new ArrayList<>(List.of(args));
+            } else {
+                System.out.println("Enter your numbers (enter q to finish entering numbers)");
+                Scanner scanner = new Scanner(System.in);
+                for (int i = 0; i < 6; i++) {
+                    System.out.print("[" + i + "] number: ");
+                    String input = scanner.nextLine();
+                    if (input.equals("q"))
+                        break;
+                    arguments.add(input);
+                }
             }
+            ArgumentParser parser = new ArgumentParser(arguments);
+            badNumbers = parser.numbers(6);
+            String data = gson.toJson(badNumbers);
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file.getName());
+            writer.write(data);
+            writer.close();
+        } else {
+            Scanner reader = new Scanner(file);
+            String data = reader.nextLine();
+            TypeToken<ArrayList<Short>> shortList = new TypeToken<>(){};
+            badNumbers = gson.fromJson(data, shortList);
         }
-        ArgumentParser parser = new ArgumentParser(arguments);
-        List badNumbers = parser.numbers(6);
         Game game = new ClassicGame(badNumbers);
         for (short num : game.generateNumbers()) {
             System.out.print(num + " ");
